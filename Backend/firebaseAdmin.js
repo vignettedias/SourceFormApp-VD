98160f8c7171abac1b5902cc0a42f9d25fd6c2f7
Vighnesh Das <dasvighnesh@gmail.com>
2026-06-01 01:52:56 +0530

    Phase 4B Firebase Storage integration Addon1

--- Backend/firebaseAdmin.js
+++ Backend/firebaseAdmin.js
@@ -4,4 +4,7 @@ admin.initializeApp({
     storageBucket: "sourceformcloud.firebasestorage.app"
 })
 
+console.log("Firebase Admin Initialized")
+console.log("Storage Bucket:", admin.app().options.storageBucket)
+
 module.exports = admin
